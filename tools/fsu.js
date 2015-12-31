db2c2551fab4df1126f70bb0954ee4f963a92606
Moos <mooster@42at.com>
2015-12-30 22:15:35 -0800

    Pipe from  STDIN using "fsu -" -- support multiple lines.

--- tools/fsu.js
+++ tools/fsu.js
@@ -13,22 +13,42 @@ var phrase = process.argv[2];
 
 if (phrase === '-') {
   // pipe from stdin
-  getStdin().then(run);
+  getStdin().then(function(lines){
+    // execute lines in sequence
+    lines.split('\n')
+      .filter(function(line){
+        return !!line.trim();
+      })
+      .map(makePromise)
+      .reduce(function(next, fn){
+        return next = next.then(fn);
+      }, Promise.resolve());
+  });
 } else {
   run(phrase);
 }
 
-function run(phrase) {
+function run(phrase, cb) {
   var fuckShitUp = createFuckShitUp({
     useAlternativeModifiers: true
   });
 
-  fuckShitUp(phrase, displayResult);
+  fuckShitUp(phrase, cb || displayResult);
+}
 
-  function displayResult(error, result) {
-    if (error) {
-      console.log(error);
-    }
-    console.log(result);
+function displayResult(error, result) {
+  if (error) {
+    console.log(error);
   }
+  console.log(result);
+}
+
+function makePromise(line) {
+  return new Promise(function (resolve, reject) {
+    run(line, function (error, result) {
+      displayResult(error, result);
+      if (error) reject(error);
+      else resolve(result);
+    });
+  });
 }
